e67862e6b5fda0f19495f1a5c6669b25f65af001
felttl <68735155+felttl@users.noreply.github.com>
2026-01-21 20:50:49 +0100

    fix problems of characters stuck in the code and improve initial architecture

---
+++ TPs/TP1_refactoring/refactored/src/main/java/refactored/Customer.java
@@ -0,0 +1,81 @@
+package refactored;
+
+import java.util.ArrayList;
+
+public class Customer {
+	private String name;
+	private ArrayList<Rental> rentals = new ArrayList<>();
+
+	public Customer(String name) {
+		this.name = name;
+	}
+
+	public void addRental(Rental rental) {
+		this.rentals.add(rental);
+	}
+
+	public String statement() {
+		double totalAmount = 0;
+		int frequentRenterPoints = 0;
+		StringBuilder result = new StringBuilder(
+			String.format(
+				"Rental Record for %s\n",
+				this.getName()
+			)
+		);
+		int index = 0;
+		for (; index < this.rentals.size(); index++) {
+			Rental current = rentals.get(index);
+			if (current.canEarnExtraPoints())
+				frequentRenterPoints++;
+			result.append(current.toString());
+			totalAmount += current.getPointsAmount();
+		}
+		result.append(
+			String.format(
+				"Amount owned is %f\n", 
+				totalAmount
+			)
+		);
+		// 2 ways of seeing things : 
+		// 1 : use only append() on the stringBuilder (difficul to see String final rendering)
+		// 2 : use String.format() to see in a better way, string organization
+		result.append(
+			String.format(
+				"You earned %d \" frequent renter points\"", 
+				-1+index+frequentRenterPoints
+			)
+		);
+		return result.toString();
+	}
+	/**
+	 * permet d'obtenir une description du statement en français
+	 * 
+	 * (containte du prof d'avoir un affichage spécifique)
+	 */
+	public String statementFR(){
+		StringBuilder result = new StringBuilder(String.format(
+			"%s a loue des films:\n", 
+			this.name
+		));
+		final int leng = this.rentals.size();
+		Rental current;
+		for (int i = 0; i < leng; i++) {
+			current = rentals.get(i);
+			result.append(current.toStringFR())
+			 .append(leng > 0 && i != leng-1 ? "," : ".")	
+			 .append("\n");
+		}
+		return result.toString();
+	}
+
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+}
