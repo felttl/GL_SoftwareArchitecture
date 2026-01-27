4af8fbf1727bbeff032d100a4bd1daa7b3584940
felttl <68735155+felttl@users.noreply.github.com>
2026-01-27 11:38:32 +0100

    fix issues and improve jacoco bash run script (macos & linux) and a few more things (compat vscode...)

--- TPs/TP1_refactoring/refactored/src/test/java/main/RentalTest.java
+++ TPs/TP1_refactoring/refactored/src/test/java/main/RentalTest.java
@@ -1,7 +1,16 @@
-package refactored;
+package main;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import refactored.Movie;
+import refactored.MovieState;
+import refactored.Rental;
 
 public class RentalTest {
 
@@ -32,7 +41,7 @@ public class RentalTest {
     public void getMovieNullTest(){
         final Movie movie = new Movie(
             "text",
-            null
+            MovieState.CHILDRENS
         );
         final int days = 16565422;
         final Rental rental = new Rental(movie, days);
@@ -40,15 +49,97 @@ public class RentalTest {
     }
 
     @Test
-    public void getStringFromMovie(){
+    public void getStringFromMovieTest(){
         final String title = "chabarbsu aiubzi";
         final Movie movie = new Movie(
             title,
-            null
+            MovieState.CHILDRENS
         );
         final int days = 16565422;
         final Rental rental = new Rental(movie, days);
         assertEquals(rental.getMovie().getTitle(), title);
     }
 
+    @Test
+    public void canEarnExtraPointsTest(){
+        final String title = "osef";
+        Movie movie = new Movie(title,MovieState.CHILDRENS);
+        final int days = 165652;
+        final short testNumber = 10;
+        final var types = new ArrayList<MovieState>(
+            Arrays.asList(
+                MovieState.CHILDRENS,
+                MovieState.NEW_RELEASE,
+                MovieState.REGULAR
+            )
+        );
+        Rental rental;        
+        int rdDays;
+        MovieState rdType;
+        int rdt;
+        Random random = ThreadLocalRandom.current();
+        boolean cond;
+        for (short idx = 0; idx < testNumber; idx++) {
+            rdt = random.nextInt(types.size());
+            rdType = types.get(rdt);
+            rdDays = random.nextInt(100);
+            movie = new Movie(title, rdType);
+            rental = new Rental(movie, rdDays);
+            cond = rdType == MovieState.NEW_RELEASE && rdDays > 1;
+            assertEquals(rental.canEarnExtraPoints(), cond); 
+        }
+    }
+    @Test
+    public void getPointsAmountTest(){
+        final String title = "osef";
+        Movie movie = new Movie(title,MovieState.CHILDRENS);
+        final short testNumber = 10;
+        final var types = new ArrayList<MovieState>(
+            Arrays.asList(
+                MovieState.CHILDRENS,
+                MovieState.NEW_RELEASE,
+                MovieState.REGULAR
+            )
+        );
+        Rental rental;
+        int rdDays;
+        MovieState rdType;
+        int rdt;
+        Random random = ThreadLocalRandom.current();
+        int computedPoints;
+        final double junitDelta = 0.0001;
+        final double factor = 1.5d;
+        for (short idx = 0; idx < testNumber; idx++) {
+            rdt = random.nextInt(types.size());
+            rdType = types.get(rdt);
+            rdDays = random.nextInt(100);
+            movie = new Movie(title, rdType);
+            rental = new Rental(movie, rdDays);
+            double amount = 0d;
+            int daysRented = rental.getDaysRented();
+            final var rtPriceCode = rental.getMovie().getPriceCode();
+            if (null != rtPriceCode) switch (rtPriceCode) {
+                case REGULAR:
+                    amount += 2;
+                    if(daysRented > 2)
+                        amount += daysRented * factor - 3;
+                    break;
+                case NEW_RELEASE:
+                    amount += daysRented * factor * 2;
+                    break;
+                case CHILDRENS:
+                    amount += 1.5;
+                    if (daysRented > 3)
+                        amount += daysRented * factor - 4.5;
+                    break;
+                default:
+                    break;
+            }
+            assertEquals(
+                rental.getPointsAmount(), 
+                amount,
+                junitDelta
+            );            
+        }
+    }
 }
